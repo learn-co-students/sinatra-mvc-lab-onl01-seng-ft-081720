df5c5304dc7d70645917d33d99260aa72a827226
Eli Cusic <tntitans31@gmail.com>
2020-09-26 17:10:24 -0500

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+class PigLatinizer
+    def latinize(word)
+        word.start_with?(/[aeiou]/i) ? vowel(word) : consonant(word)
+    end
+
+    def piglatinize(words)
+        words.split(' ').map{|word| latinize(word)} .join(" ")
+    end
+
+    def consonant(word)
+        array = word.split('')
+        if !word.start_with?(/[aeiou]/i)
+            cons = array[0]
+            array.shift
+            array << cons 
+            consonant(array.join(''))
+        else
+            word + 'ay'
+        end
+    end
+
+    def vowel(word)
+        word + 'way'
+    end
+end
