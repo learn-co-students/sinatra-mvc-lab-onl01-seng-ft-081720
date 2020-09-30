80c64645bfc632177c708085c86415fbec5ec7de
Tabatha Slatton <valbatha@gmail.com>
2020-09-29 22:31:02 -0600

    Done.

--- app.rb
+++ app.rb
@@ -5,4 +5,10 @@ class App < Sinatra::Base
     get "/" do
         erb :user_input
     end
+
+    post "/piglatinize" do
+        @piglatin = PigLatinizer.new
+        @phrase =   params[:user_phrase]
+        erb :piglatinize
+    end
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,14 +1,23 @@
 class PigLatinizer
-    attr_reader :text
 
-    def initialize(text)
-        @text = text
-    end
-
-    def piglatin
-        @text.split.each do |word|
-         word.split("")
-
+    def piglatinize(text)
+        piglatin = []
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        text.split.each do |word|
+            letters = word.split("")
+            if vowels.include?(word[0].downcase)
+                letters << ["w", "a", "y"]
+                piglatin << letters.join
+            else
+               word_array = word.split /([aeiou].*)/
+                consonants = word_array[0]
+                root = word_array[1]
+                pig = "ay"
+                new_word = [root, consonants, pig].join
+                piglatin << new_word
+            end
         end
+        piglatin.join(" ")
     end
+
 end
