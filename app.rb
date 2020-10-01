0021290c480ebea287cccacb978786bf91a0aa1f
“saschakala” <mxsaschakala@gmail.com>
2020-09-30 18:36:34 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @piglatin = PigLatinizer.new
+        @answer = @piglatin.piglatinize(params[:user_phrase])
+    
+        erb :piglatinize
+    end
+
+    # post '/piglatinize' do
+    #     @latinized_text = PigLatinizer.new(params[:user_phrase])
+    #     erb :piglatinize
+    # end
+
 end
